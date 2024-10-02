2dd50f620b374d44406717f8620e1e3e674e80b2
Amboara <amboara@misaina.com>
2024-10-02 11:49:32 +0300

    list todo

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -13,7 +13,9 @@ class TodoController extends AbstractController
     #[Route('/todo', name: 'todo.index')]
     public function index(Request $request): Response
     {
-        return new Response('Liste des taches pour '.$request->query->get('nom', 'inconnu'));
+        return $this->render('todo/index.html.twig', [
+            'tasks' => ['Tache 1', 'Tache 2', 'Tache 3']
+        ]);
     }
 
     #[Route('/todo/{slug}-{id}', name: 'todo.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
