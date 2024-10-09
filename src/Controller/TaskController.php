950693c3d673b4e4815358307faa8d3918d53c4d
Amboara <amboara@misaina.com>
2024-10-09 08:28:22 +0300

    tp 3c : findByFilters

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -13,13 +13,23 @@ class TaskController extends AbstractController
     #[Route('/task', name: 'task.index')]
     public function index(Request $request, TaskRepository $repository): Response
     {
-        $tasks = $repository->findAll(4);
-        $totalEstimates = $repository->findTotalEstimates(4);
+        // Récupérer les valeurs de recherche et de filtre min/max
+        $searchTitle = $request->query->get('search', '');
+        $minEstimate = $request->query->get('min_estimate', 0);
+        $maxEstimate = $request->query->get('max_estimate', 10000); // Limite par défaut
+
+        // Appeler la méthode du repository pour filtrer les tâches
+        $tasks = $repository->findByFilters($searchTitle, $minEstimate, $maxEstimate);
+        $totalEstimates = $repository->findTotalEstimates();
 
         return $this->render('task/index.html.twig', [
             'tasks' => $tasks,
-            'totalEstimates' => $totalEstimates
+            'totalEstimates' => $totalEstimates,
+            'searchTitle' => $searchTitle,
+            'minEstimate' => $minEstimate,
+            'maxEstimate' => $maxEstimate,
         ]);
+
     }
 
     #[Route('/task/{slug}-{id}', name: 'task.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
