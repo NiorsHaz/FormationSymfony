0a458dc7acb483f1b360029a6246734204c15c48
Amboara <amboara@misaina.com>
2024-10-15 09:36:49 +0300

    tp 7b : fix paginator

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -10,11 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ProjectController extends AbstractController
 {
-    #[Route('/projects', name: 'project:index')]
+    #[Route('/projects', name: 'project.index')]
     public function index(Request $request, ProjectRepository $repository): Response
     {
         $page = $request->query->getInt('page', 1);
-        $limit = 2;
+        $limit = 1;
         $projects = $repository->paginateProjects($page, $limit);
         $maxPage = ceil($projects->count() / $limit);
         return $this->render('project/index.html.twig', [
